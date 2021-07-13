65218ba975bc58bd6b46aa8de04b0307e818f8d3
agnishg <55572740+agnishg@users.noreply.github.com>
2021-07-13 22:05:14 +0200

    up

--- js/scrolltracker.js
+++ js/scrolltracker.js
@@ -23,3 +23,20 @@ window.addEventListener('scroll',
         scrollIndicatorElt.style.width
                  = percentage + '%';
 }
+
+function addEvent(obj, evt, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(evt, fn, false);
+    } else if (obj.attachEvent) {
+        obj.attachEvent("on" + evt, fn);
+    }
+}
+
+addEvent(document, 'mouseout', function (evt) {
+    var modalId = 'exitModal';
+    if (evt.toElement === null && evt.relatedTarget === null && !localStorage.getItem('modal.' + modalId)) {
+        var modal = new bootstrap.Modal('#' + modalId);
+        modal.show();
+        localStorage.setItem('modal.' + modalId, '1');
+    }
+});
